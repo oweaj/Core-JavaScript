fcafe610a1348e5836a6563b1d8b991ef0b4b3f0
oweaj <qwe1520156@naver.com>
2023-01-13 17:12:03 +0900

    Alert창 is-active 유틸 함수

---
+++ client/lib/dom/showAlert.js
@@ -0,0 +1,16 @@
+import { getNode } from "./getNode.js";
+import { addClass, removeClass } from "./css.js";
+
+export function showAlert(node, text = "에러입니다!!", timeout = 1500) {
+  if (typeof node === "string") {
+    node = getNode(node);
+  }
+  node.textContent = text;
+
+  addClass(node, "is-active");
+  setTimeout(() => {
+    removeClass(node, "is-active");
+  }, timeout);
+}
+
+// showAlert(".alert", "올바른 정보를 확인해주세요", 2000);
