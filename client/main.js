3ac91e49d2b59220591db07a492b14dc35357c14
oweaj <qwe1520156@naver.com>
2023-01-16 10:55:53 +0900

    주사위 버튼 클릭 작동 event 적용

--- client/main.js
+++ client/main.js
@@ -1,1 +1,21 @@
-console.log(window);
+import { diceAnimation, getNode } from "./lib/index.js";
+
+// [ 주사위 굴리기 ]
+// 1. dice 애니메이션 불러오기
+// 2. bindEvent 유틸함수 만들기
+// 3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
+// 4. 변수 보호를 위한 클로저 + IIFE 사용하기
+
+const rollingBtn = getNode(".buttonGroup > button:nth-child(1)");
+
+let stopAnimation;
+
+const handlerRollingDice = (e) => {
+  // let target = e.target;
+
+  stopAnimation = setInterval(diceAnimation, 1000);
+
+  // clearInterval(e);
+};
+
+rollingBtn.addEventListener("click", handlerRollingDice);
