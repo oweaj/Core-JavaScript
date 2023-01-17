8730d501687da27fc29ec52b71151aad86f404cb
oweaj <qwe1520156@naver.com>
2023-01-17 16:06:44 +0900

    [1.17일] 서버통신 ajax XHR 실습

--- client/main.js
+++ client/main.js
@@ -1,1 +1,11 @@
-console.log(window);
+import { xhrData, insertLast } from "./lib/index.js";
+
+// xhrData.get(
+//   "https://jsonplaceholder.typicode.com/users/1",
+//   (result) => {
+//     insertLast("body", JSON.stringify(result));
+//   },
+//   (error) => {
+//     insertLast("body", "데이터 로딩에 실패했습니다!!");
+//   }
+// );
