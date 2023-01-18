600f6f19a2949497c08c7c537306ea8de920f74c
oweaj <qwe1520156@naver.com>
2023-01-18 16:07:29 +0900

    [1.18일] oweaj 사용

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-import { xhrData, insertLast } from "./lib/index.js";
+import { xhrData, insertLast, xhrPromise, oweaj } from "./lib/index.js";
 
 // xhrData.get(
 //   "https://jsonplaceholder.typicode.com/users/1",
@@ -9,3 +9,19 @@ import { xhrData, insertLast } from "./lib/index.js";
 //     insertLast("body", "데이터 로딩에 실패했습니다!!");
 //   }
 // );
+
+// xhrPromise
+//   .get("https://jsonplaceholder.typicode.com/users/2")
+//   .then((result) => {
+//     insertLast(document.body.JSON.stringify(result));
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+async function render() {
+  let response = await oweaj.get("https://jsonplaceholder.typicode.com/users/3");
+  console.log(response.data);
+}
+
+render();
